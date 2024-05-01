6716b9a83516ec83be05b6c331cec638a2afb891
sabshr <shreeramsabareesan@gmail.com>
2024-05-01 04:14:14 -0700

    e

--- swellingPlot.py
+++ swellingPlot.py
@@ -14,21 +14,16 @@ medians = [np.median(pva_ca), np.median(pva_ca_bcd_4_1), np.median(pva_ca_bcd_2_
 # Combine data
 all_data = np.concatenate([pva_ca, pva_ca_bcd_4_1, pva_ca_bcd_2_1, pva_bcd_4_1])
 
-# Initialize graph
+# initialization
 labels = ['PVA-CA'] * len(pva_ca) + ['4:1 PVA-CA-BCD'] * len(pva_ca_bcd_4_1) + ['2:1 PVA-CA-BCD'] * len(pva_ca_bcd_2_1) + ['4:1 PVA-BCD'] * len(pva_bcd_4_1)
-plt.figure(figsize=(10, 6))  # Adjust figure size if needed
-
-# Create categorical scatter plot with median lines
-sns.stripplot(x=labels, y=all_data, jitter=True, alpha=0.5, marker='o')
-
-# Plot median lines
-for i, median in enumerate(medians):
-    plt.plot([i - 0.2, i + 0.2], [median, median], color='black', lw=1)
-
-# Title and labels
+plt.figure(figsize=(10, 6))  # Adjust figure size for slides/paper/poster
+sns.stripplot(x=labels, y=all_data, jitter=True, alpha=0.5, marker='o')  #categorical scatter plot
 plt.title('PVA Gel Swelling %')
 plt.ylabel('Swelling Capacity (%wt)')
 plt.xlabel('Tests')
 
-# Show plot
+# median lines
+for i, median in enumerate(medians):
+    plt.plot([i - 0.2, i + 0.2], [median, median], color='black', lw=1)
+
 plt.show()
